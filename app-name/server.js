53120b8078629b1254e7401c088b1bc95173fe2d
zombico <franz@ombi.co>
2020-12-08 21:46:06 -0500

    with react app

---
+++ app-name/server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const path = require("path");
+
+
+app.use('/', express.static(
+  path.join(__dirname, '../build'))
+)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+
+module.exports = app;
